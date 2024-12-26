267d15f60e32e4ddb03a01c2e97bea28302e36bd
Antonio Martínez <antonio@weffective.com>
2024-12-26 20:08:32 +0100

    fix: amf naming

--- services/iot-devices/cmd/di/di_dynamic_parameters.go
+++ services/iot-devices/cmd/di/di_dynamic_parameters.go
@@ -3,22 +3,22 @@ package di
 import (
 	"fmt"
 
-	fgu_dynamic_parameter "github.com/AntonioMartinezFernandez/services/iot-devices/pkg/dynamic-parameter"
-	fgu_http_server "github.com/AntonioMartinezFernandez/services/iot-devices/pkg/http-server"
+	amf_dynamic_parameter "github.com/AntonioMartinezFernandez/services/iot-devices/pkg/dynamic-parameter"
+	amf_http_server "github.com/AntonioMartinezFernandez/services/iot-devices/pkg/http-server"
 )
 
 const changeDynamicParameterJsonSchemaFileName = "change-dynamic-parameter.schema.json"
 
 type DynamicParameterServices struct {
-	DynamicParameterRetriever *fgu_dynamic_parameter.DynamicParameterRetriever
-	StaticApiKeyStorage       []fgu_http_server.StaticApiKey
+	DynamicParameterRetriever *amf_dynamic_parameter.DynamicParameterRetriever
+	StaticApiKeyStorage       []amf_http_server.StaticApiKey
 }
 
 func InitDynamicParameterServices(commonServices *CommonServices, httpServices *HttpServices) *DynamicParameterServices {
-	repository := fgu_dynamic_parameter.NewRedisDynamicParameterRepository(commonServices.RedisClient)
-	retriever := fgu_dynamic_parameter.NewDynamicParameterRetrieverFromConfigFile(repository, commonServices.Config.DynamicParametersFilePath)
+	repository := amf_dynamic_parameter.NewRedisDynamicParameterRepository(commonServices.RedisClient)
+	retriever := amf_dynamic_parameter.NewDynamicParameterRetrieverFromConfigFile(repository, commonServices.Config.DynamicParametersFilePath)
 
-	fgu_dynamic_parameter.RegisterDynamicParameterBusesOperations(
+	amf_dynamic_parameter.RegisterDynamicParameterBusesOperations(
 		commonServices.UlidProvider,
 		retriever,
 		repository,
@@ -26,7 +26,7 @@ func InitDynamicParameterServices(commonServices *CommonServices, httpServices *
 		commonServices.QueryBus,
 	)
 
-	staticApiKeys := fgu_http_server.StaticApiKeysFromPipedString(commonServices.Config.DynamicParametersApiKeys)
+	staticApiKeys := amf_http_server.StaticApiKeysFromPipedString(commonServices.Config.DynamicParametersApiKeys)
 
 	dynamicParametersServices := &DynamicParameterServices{
 		DynamicParameterRetriever: retriever,
@@ -43,13 +43,13 @@ func registerDynamicParameterRoutes(
 	commonServices *CommonServices,
 	httpServices *HttpServices,
 ) {
-	staticApiKeysMiddleware := fgu_http_server.NewApiKeyValidationMiddleware(
+	staticApiKeysMiddleware := amf_http_server.NewApiKeyValidationMiddleware(
 		httpServices.JsonApiResponseMiddleware,
-		fgu_http_server.WithLogger(commonServices.Logger),
-		fgu_http_server.WithKeysByOwner(dynamicParametersServices.StaticApiKeyStorage...),
+		amf_http_server.WithLogger(commonServices.Logger),
+		amf_http_server.WithKeysByOwner(dynamicParametersServices.StaticApiKeyStorage...),
 	)
 
-	changeDynamicParameterJsonSchemaValidator := fgu_http_server.NewRequestValidatorMiddleware(
+	changeDynamicParameterJsonSchemaValidator := amf_http_server.NewRequestValidatorMiddleware(
 		httpServices.JsonApiResponseMiddleware,
 		fmt.Sprintf(
 			"%s/%s/%s",
@@ -61,7 +61,7 @@ func registerDynamicParameterRoutes(
 
 	httpServices.Router.Get(
 		"/system/parameter/{parameterName}",
-		fgu_dynamic_parameter.HandleGetDynamicParameter(
+		amf_dynamic_parameter.HandleGetDynamicParameter(
 			commonServices.QueryBus,
 			httpServices.JsonApiResponseMiddleware,
 		),
@@ -70,7 +70,7 @@ func registerDynamicParameterRoutes(
 
 	httpServices.Router.Put(
 		"/system/parameter/{parameterName}",
-		fgu_dynamic_parameter.HandleChangeDynamicParameter(
+		amf_dynamic_parameter.HandleChangeDynamicParameter(
 			commonServices.CommandBus,
 			httpServices.JsonApiResponseMiddleware,
 		),
